49421c34e0ac8e9bb8faab7d9fee7b602d40af60
José Junior <juhsantos.goleiro634@gmail.com>
2025-02-14 07:58:40 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     console.log('Compilando SCSS para CSS...')
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/main.scss')
     .pipe(sass({ outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 };
 
-exports.default = styles;
+function images() {
+    console.log('Minificando imagens...')
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+};
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 };
