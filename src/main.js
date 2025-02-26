daa1fe235c514c8dc5127bad3727b9dbb8d68227
José Junior <juhsantos.goleiro634@gmail.com>
2025-02-26 07:50:16 -0300

    criação das seções baixe filmes e séries, dispositivos, perguntas frequentes com interações feitas em javascript e criação do rodapé

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button_active) {
@@ -11,7 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
             button_active.target.classList.add('shows__tabs__button--is-active');
         });
     };
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 });
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    console.log("Tudo certo!");
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+};
 
 function removeButtonActive() {
     const buttons = document.querySelectorAll('[data-tab-button]');
