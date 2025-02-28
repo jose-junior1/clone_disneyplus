bc4a7e5709983aab5f5328cf487b7d81e2a81f24
José Junior <juhsantos.goleiro634@gmail.com>
2025-02-28 08:25:54 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const heightHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heightHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    });
     
+
+    // Troca seção no catálogo, e identifica qual está sendo exibida
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button_active) {
             const targetTab = button_active.target.dataset.tabButton;
@@ -13,10 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     };
 
+    // Seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 });
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+};
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+};
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
